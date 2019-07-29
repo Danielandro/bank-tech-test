8e44b351631a0024484f714c52873fdde7f4db3c
Danny <mtme13@hotmail.com>
2019-07-29 13:25:00 +0100

    test: balance is zero when account is instantiated

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,12 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
-  
+
   it 'an account can be instantiated' do
     expect(account).not_to be(nil)
+  end
+
+  it 'account has a balance of 0 when created' do
+    expect(account.balance).to eq(0)
   end
 end
