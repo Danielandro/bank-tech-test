03acd03011236d9725533ca581cf621439482e7c
Danny <mtme13@hotmail.com>
2019-07-30 10:16:25 +0100

    test: transaction saves the date when its created

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,14 @@
 class Transaction
 
   attr_reader :credit, :debit, :balance
-  def initialize(credit = 0, debit = 0, balance:)
-    # @time = Time.new.strftime("%d/%m/%Y")
+  def initialize(credit = 0, debit = 0, balance:, time: Time)
+    @time = time.new
     @credit = credit
     @debit = debit
     @balance = balance
   end  
+
+  def time 
+    @time
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,14 +7,22 @@ describe Transaction do
       expect(transaction.balance).to eq(10)
     end
     
-    it 'has default credit of 0' do
+    it 'has default credit amount of 0' do
       transaction = Transaction.new(balance: 10)
       expect(transaction.credit).to eq(0)
     end
 
-    it 'has default debit of 0' do
+    it 'has default debit amount of 0' do
       transaction = Transaction.new(balance: 10)
       expect(transaction.debit).to eq(0)
+    end
+    
+    it 'saves the date of the transaction' do
+      current_time = "2019-07-30 10:01:25 +0100"
+      allow(Time).to receive(:new) { current_time }
+
+      transaction = Transaction.new(balance: 10, time: Time)
+      expect(transaction.time).to eq(current_time)
     end
   end
 end
