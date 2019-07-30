f435e2d1b07baa57e3f239963e943d90f36c15ab
Danny <mtme13@hotmail.com>
2019-07-30 10:35:15 +0100

    refactor: use attribute reader for time / reduce repetition in transaction specs

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,14 +1,11 @@
 class Transaction
 
-  attr_reader :credit, :debit, :balance
+  attr_reader :credit, :debit, :balance, :time
   def initialize(credit = 0, debit = 0, balance:, time: Time)
     @time = time.new
     @credit = credit
     @debit = debit
     @balance = balance
-  end  
-
-  def time 
-    @time
   end
+  
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,27 +2,28 @@ require 'transaction'
 
 describe Transaction do
   describe 'When instatiated' do
+    before :each do
+      @transaction = Transaction.new(balance: 10)
+    end
+    
     it 'takes a balance amount' do
-      transaction = Transaction.new(balance: 10)
-      expect(transaction.balance).to eq(10)
+      expect(@transaction.balance).to eq(10)
     end
     
     it 'has default credit amount of 0' do
-      transaction = Transaction.new(balance: 10)
-      expect(transaction.credit).to eq(0)
+      expect(@transaction.credit).to eq(0)
     end
 
     it 'has default debit amount of 0' do
-      transaction = Transaction.new(balance: 10)
-      expect(transaction.debit).to eq(0)
+      expect(@transaction.debit).to eq(0)
     end
     
     it 'saves the date of the transaction' do
       current_time = "2019-07-30 10:01:25 +0100"
       allow(Time).to receive(:new) { current_time }
 
-      transaction = Transaction.new(balance: 10, time: Time)
-      expect(transaction.time).to eq(current_time)
+      new_transaction = Transaction.new(balance: 10, time: Time)
+      expect(new_transaction.time).to eq(current_time)
     end
   end
 end
